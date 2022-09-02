943e24819b07f96358d2823225e13d3db2274bff
fnvn <fnmarambio@gmail.com>
2022-09-02 01:23:48 -0400

    feat: add colors and fonts

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="box">
+        <h3>navbar</h3>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/views/Home/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>SMOOTH-FOLIO</h1>
+    </>
+  );
+}
+
+export default Home;
