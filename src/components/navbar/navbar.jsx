3a76be180d1dd8f7c7615db745161e6092accacf
fnvn <fnmarambio@gmail.com>
2022-09-04 04:59:28 -0300

    feat: add basic navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,19 @@
 import "./navbar.css";
+import { BiUserPin, BiAdjust } from "react-icons/bi";
+import "tailwindcss/tailwind.css";
+import logoFranma from "./logo_blue.png";
 
 function Navbar() {
   return (
     <>
-      <div className="box">
-        <h3>navbar</h3>
+      <div className="place-content-between flex bg-[#202225] px-5 md:px-10 py-3 mt-5 mb-3 rounded-md items-center ">
+        <div>
+          <img src={logoFranma} alt="" className="h-[1.6rem]" />
+        </div>
+        <div className="flex gap-4">
+          <BiAdjust size="1.6rem" />
+          <BiUserPin size="1.6rem" />
+        </div>
       </div>
     </>
   );
