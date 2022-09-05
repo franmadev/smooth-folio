af64d41d56b18a83f0c9dfdc8bc3b68c2ffdd62e
fnvn <fnmarambio@gmail.com>
2022-09-05 02:17:01 -0300

    feat: add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkgrey: "#202225",
+        grey: "#36393f",
+        softgrey: "#3c3f45",
+        primary: "#218bd7",
+        secundary: "#b667e3",
+        softwhite: "#CCCCCC",
+      },
+    },
   },
   plugins: [],
 };
