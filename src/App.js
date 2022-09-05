cbfcdd483b7134bbcd1440e5a305a270edb47288
fnvn <fnmarambio@gmail.com>
2022-09-05 02:17:50 -0300

    feat: create and add footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "tailwindcss/tailwind.css";
 import Home from "./views/Home/home";
 import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import "./styles/global.css";
 
 function App() {
   return (
-    <div className="max-w-screen-xl md:mx-auto  mx-4">
-      <Navbar />
-      <Home />
-    </div>
+    <>
+      <div className="max-w-screen-xl md:mx-auto  mx-4">
+        <Navbar />
+        <Home />
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,25 @@
+import logoFranma from "../../assets/icons/logo/logo_blue.png";
+function Footer() {
+  return (
+    <>
+      <section className="bg-darkgrey">
+        <div className="mx-4">
+          <div></div>
+          <div className="place-content-center flex py-6">
+            <div className="ml-[-13px]">
+              <img
+                src={logoFranma}
+                alt=""
+                className="w-32 place-content-center"
+              />
+              <h3 className="text-center py-2">franma.dev - 2022</h3>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Footer;
